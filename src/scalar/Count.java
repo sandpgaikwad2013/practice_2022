74b91241178574161c0a7c22c120ffbf6d1a88a1
aarav <sandpgaikwad2013@gmail.com>
2022-06-22 21:36:45 +0530

    updated comment

--- src/scalar/Count.java
+++ src/scalar/Count.java
@@ -2,4 +2,30 @@ package scalar;
 
 //given an array count the number of elements which has element which is greater than itself
 public class Count {
+    public static void main(String[] args) {
+        approachOne();
+        approachTwo();
+    }
+
+    private static void approachTwo() {
+        int counter =0;
+        int [] a={4,7,2,9};
+        for (int i:a
+             ) {
+            for (int j:a
+                 ) {
+                if(a[i]<a[j]){
+                    counter++;
+                }
+            }
+
+        }
+        System.out.println(counter);
+
+
+
+    }
+
+    private static void approachOne() {
+    }
 }
