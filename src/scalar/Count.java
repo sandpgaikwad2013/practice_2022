32213701f57ccc6467eef9c35647849db04e213f
aarav <sandpgaikwad2013@gmail.com>
2022-06-22 09:10:28 +0530

    updated comment

---
+++ src/scalar/Count.java
@@ -0,0 +1,5 @@
+package scalar;
+
+//given an array count the number of elements which has element which is greater than itself
+public class Count {
+}
---
+++ src/scalar/SumOfArrayElements.java
@@ -0,0 +1,4 @@
+package scalar;
+//given an array N , print the index position of two elements in such way that i+j=k where k is 20
+public class SumOfArrayElements {
+}
